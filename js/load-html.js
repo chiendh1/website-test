f5d4c2996ddae8737ed99f0a27cc42820d906f60
minhpv90 <54697479+minhpv90@users.noreply.github.com>
2022-02-15 16:01:45 +0700

    Fix bug

--- js/load-html.js
+++ js/load-html.js
@@ -1,3 +1,3 @@
-$(".about").not('.contact').load("/about.html");
+$(".about").not('.contact-html').load("/about.html");
 $(".ui").load("/header.html");
-$(".contact").load("/contact.html");
+$(".contact-html").load("/contact.html");
